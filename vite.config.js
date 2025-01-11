b4114abb9973a7e72c0a2ea0796bd1f5f09aeda1
bmkaradia <bmk302006@gmail.com>
2025-01-11 13:10:47 +0530

    update for dist folder

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist', // Ensure this is set to 'dist'
+    rollupOptions: {
+      input: '/home/ratankhichifullappcode/FontWebSite/index.html', // Path to your source index.html before the build
+    },
+  },
   server: {
     port: 3001 // Change this to the desired port number
   },
